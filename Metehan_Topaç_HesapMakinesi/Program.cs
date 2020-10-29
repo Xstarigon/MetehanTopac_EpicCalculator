d1b5ac85c9a787b7bf7b5bdc817e2fb3d159b719
Metahan Topaç <metehan.topac02@gmail.com>
2020-10-29 15:16:13 +0300

    EpicCalculatorV1

---
+++ Metehan_Topaç_HesapMakinesi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metehan_Topaç_HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
